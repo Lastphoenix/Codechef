c8ac1917cb9993e5b15cfddb2f93c8a1a9f5d57c
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-06-28 11:47:14 -0300

    AC
    AC

---
+++ beginner/chefandfruits/com/FRUIT.java
@@ -0,0 +1,36 @@
+package chefandfruits.com;
+
+import java.util.Scanner;
+
+//https://www.codechef.com/problems/FRUITS
+
+public class FRUIT {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while (t > 0) {
+		
+		int apples = sc.nextInt();
+		int oranges = sc.nextInt();
+		int coins = sc.nextInt();
+			
+			for (int i = 0; i < coins; i++) {
+				if (apples < oranges) {
+					apples++;
+				}
+				if (apples > oranges) {
+					oranges++;
+				} 
+				if (apples == oranges) {
+					break;
+				}
+			}
+		System.out.println(Math.abs(oranges - apples));
+		t--;
+		}
+		sc.close();
+	}
+}
