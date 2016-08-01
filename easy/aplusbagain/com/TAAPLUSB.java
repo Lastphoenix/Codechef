7e3cae1d0554ce17078c67d067f0754f17221ad0
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-08-01 17:48:44 -0300

    coding

--- easy/aplusbagain/com/TAAPLUSB.java
+++ easy/aplusbagain/com/TAAPLUSB.java
@@ -1,9 +1,20 @@
 package aplusbagain.com;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TAAPLUSB {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tests = Integer.parseInt(br.readLine());
+		
+		while (tests > 0) {
+			
+			int number = Integer.parseInt(br.readLine());
+			
+			tests--;
+		}
 	}
 }
