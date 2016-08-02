0469024fc85ef4bf946c32191e134b850976279e
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-08-01 22:26:02 -0300

    AC
    AC

--- easy/aplusbagain/com/TAAPLUSB.java
+++ easy/aplusbagain/com/TAAPLUSB.java
@@ -6,14 +6,37 @@ import java.io.InputStreamReader;
 public class TAAPLUSB {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tests = Integer.parseInt(br.readLine());
-		
+
 		while (tests > 0) {
-			
+
 			int number = Integer.parseInt(br.readLine());
-			
+
+			switch (number) {
+			case 1:
+				System.out.println(0.45);
+				break;
+			case 2:
+				System.out.println(0.945);
+				break;
+			case 3:
+				System.out.println(1.4445);
+				break;
+			case 4:
+				System.out.println(1.94445);
+				break;
+			case 5:
+				System.out.println(2.444445);
+				break;
+			case 6:
+				System.out.println(2.944444);
+				break;
+			default:
+				System.out.println((double) number / 2 - .0555555);
+			}
+
 			tests--;
 		}
 	}
