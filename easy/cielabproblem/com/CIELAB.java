23278f0f39b539464c569e3698c89f77bb5a7cb6
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-08-08 16:36:28 -0300

    AC CIELAB
    AC CIELAB

--- easy/cielabproblem/com/CIELAB.java
+++ easy/cielabproblem/com/CIELAB.java
@@ -1,9 +1,28 @@
 package cielabproblem.com;
 
+import java.io.*;
+
 public class CIELAB {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] s = br.readLine().split(" ");
+		int A = Integer.parseInt(s[0]);
+		int B = Integer.parseInt(s[1]);
+		int C = A - B;
+		if (C > 0) {
+			if (C % 10 == 9) {
+				System.out.println(A - B - 1);
+			} else {
+				System.out.println(A - B + 1);
+			}
+		} else {
+			if (C % 10 == 9) {
+				System.out.println(B - A - 1);
+			} else {
+				System.out.println(B - A + 1);
+			}
+		}
 	}
 }
