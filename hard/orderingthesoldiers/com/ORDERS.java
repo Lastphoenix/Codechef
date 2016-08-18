96440e7744c91d8490eca74210b9e71b1710b8f6
Lastphoenix <nelson_luiz_br@yahoo.com.br>
2016-08-18 15:21:50 -0300

    ...
    ...

--- hard/orderingthesoldiers/com/ORDERS.java
+++ hard/orderingthesoldiers/com/ORDERS.java
@@ -15,10 +15,12 @@ public class ORDERS {
 			int n = Integer.parseInt(br.readLine());
 			ArrayList<Integer> newPosition = new ArrayList<Integer>();
 			String[] input = br.readLine().split(" ");
+			
             for(int i = 0; i < n; i++){
                 int inputs = Integer.parseInt(input[i]);
                 newPosition.add(inputs);
             }
+            
 			String result = Arrays.toString(finalPosition(newPosition))
 					.replace(",", "")  
 				    .replace("[", "")  
@@ -38,6 +40,7 @@ public class ORDERS {
             int position = i - newPosition.get(i);
             list.add(position,i);
         }
+		
         for(int i = 0; i < list.size(); i++){
         	finalPosition[list.get(i)] = i + 1;
         }
